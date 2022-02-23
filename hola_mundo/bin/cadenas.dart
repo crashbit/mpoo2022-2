d08a1ef481549dea23f8d78796981d29fd674c89
German Santos <germans@unam.mx>
2022-02-23 15:55:37 -0600

    Cadenas

---
+++ hola_mundo/bin/cadenas.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nombre = "Jose Perez Ozuna";
+  String alumno = 'Joan Martinez';
+  String familia = "🦄";
+
+  print("$nombre $alumno $familia");
+}
--- hola_mundo/bin/hola_mundo.dart
+++ hola_mundo/bin/hola_mundo.dart
@@ -1,3 +1,17 @@
 void main() {
   print("Hola mundo");
-}
+
+  // Inferencia por tipo de dato
+  var miEntero = 10;
+  int miOtroEntero = 20;
+
+  const holamundo = "Adios mundo";
+  final horaInicio = DateTime(2022);
+} 
+
+/*
+int
+double
+dynamic
+String
+*/
