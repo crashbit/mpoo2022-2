dc5024c59dbd3d0ee4f361aa32280673ba218059
German Santos <germans@unam.mx>
2022-04-06 16:26:13 -0500

    ListView, Row y Column

---
+++ limites/android/app/src/main/kotlin/com/example/limites/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.limites
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ limites/lib/LimitesApp.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LimitesApp extends StatelessWidget {
+  const LimitesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Limites")),
+      body: ListView(
+        children: [
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "Hola mundo",
+            style: TextStyle(fontSize: 30),
+          ),
+        ],
+      ),
+    );
+  }
+}
