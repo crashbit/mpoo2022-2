0f355e31a2d3d417fe99dd9e9b4949c1cbc1ec0d
German Santos <germans@unam.mx>
2022-04-25 16:01:54 -0500

    Tarea

--- cardsflutter/lib/NuevaPagina.dart
+++ cardsflutter/lib/NuevaPagina.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
-class NuevaPagina extends StatelessWidget {
+class NuevaPagina extends StatefulWidget {
   int? numero;
   NuevaPagina({Key? key, required this.numero}) : super(key: key);
 
   @override
+  State<NuevaPagina> createState() => _NuevaPaginaState();
+}
+
+class _NuevaPaginaState extends State<NuevaPagina> {
+  @override
   Widget build(BuildContext context) {
+    int valor = 0;
+
     return Scaffold(
-      appBar: AppBar(title: Text("Nueva Pagina $numero")),
+      appBar: AppBar(title: Text("Nueva Pagina ${widget.numero}")),
+      body: Center(
+          child: Text("hola ${widget.numero}", style: TextStyle(fontSize: 40))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("toco boton, toco boton");
+          setState(() {
+            widget.numero = 100;
+          });º
+        },
+      ),
     );
   }
 }
