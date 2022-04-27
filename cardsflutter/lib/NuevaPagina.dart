43959908aede49e10728bceef5f756f051a1ba4b
German Santos <germans@unam.mx>
2022-04-27 16:19:35 -0500

    otro mensaje

--- cardsflutter/lib/NuevaPagina.dart
+++ cardsflutter/lib/NuevaPagina.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class NuevaPagina extends StatefulWidget {
   int? numero;
+
   NuevaPagina({Key? key, required this.numero}) : super(key: key);
 
   @override
@@ -11,18 +12,25 @@ class NuevaPagina extends StatefulWidget {
 class _NuevaPaginaState extends State<NuevaPagina> {
   @override
   Widget build(BuildContext context) {
-    int valor = 0;
-
     return Scaffold(
       appBar: AppBar(title: Text("Nueva Pagina ${widget.numero}")),
       body: Center(
-          child: Text("hola ${widget.numero}", style: TextStyle(fontSize: 40))),
+          child: Column(
+        children: [
+          Text("hola ${widget.numero}", style: TextStyle(fontSize: 40)),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context, "Hola");
+            },
+            child: Text("Hola"),
+          ),
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("toco boton, toco boton");
           setState(() {
-            widget.numero = 100;
-          });º
+            widget.numero = widget.numero! + 1;
+          });
         },
       ),
     );
